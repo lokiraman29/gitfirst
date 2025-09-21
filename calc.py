1f6752e26b28c8f74a58b8ffa0440e601df6d7dc
lokiraman29 <lokiraman22@gmail.com>
2025-09-21 16:15:42 +1000

    update project1.py

---
+++ calc.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Multiplication:", num1 * num2)    
